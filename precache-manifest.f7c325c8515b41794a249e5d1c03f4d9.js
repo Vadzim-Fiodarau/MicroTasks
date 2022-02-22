91b020ee28ca45092243cf2ada8fc79bdf082243
Vadzim-Fiodarau <fiodarau.v@gmail.com>
2022-02-22 18:07:19 +0300

    Updates

---
+++ precache-manifest.f7c325c8515b41794a249e5d1c03f4d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e0471b122eedcad07379b31d00eec5",
+    "url": "/MicroTasks/index.html"
+  },
+  {
+    "revision": "e59b9b889cf61cb4dd4d",
+    "url": "/MicroTasks/static/css/main.e9f5596c.chunk.css"
+  },
+  {
+    "revision": "2c4555d6a81b87decec6",
+    "url": "/MicroTasks/static/js/2.70107d92.chunk.js"
+  },
+  {
+    "revision": "83dd22b0688da01307759f0db1576406",
+    "url": "/MicroTasks/static/js/2.70107d92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e59b9b889cf61cb4dd4d",
+    "url": "/MicroTasks/static/js/main.5f665955.chunk.js"
+  },
+  {
+    "revision": "42592998ede8255506cb",
+    "url": "/MicroTasks/static/js/runtime-main.490aae00.js"
+  }
+]);
